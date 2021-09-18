2051909825bca8d78c538b83c5392283e5c7f33d
Rory Gao <30483987+biosharp18@users.noreply.github.com>
2021-09-17 23:44:58 -0400

    submit views

--- server.js
+++ server.js
@@ -3,10 +3,22 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
+const { v4: uuidV4 } = require('uuid')
+var bodyParser = require('body-parser');
+var cookieParser = require("cookie-parser");
+const { cp } = require('fs')
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+app.post('/submit', (req, res) => {
+//NEED TO REDIRECT
 })
