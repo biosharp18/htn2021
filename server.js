1f605503f54858d98c97f6368e1f80dc389dd5e1
Rory Gao <30483987+biosharp18@users.noreply.github.com>
2021-09-17 23:43:50 -0400

    HELLO WORLD

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
